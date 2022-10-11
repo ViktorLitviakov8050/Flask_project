38af231dc5153a2c301a45f53a3465773dbcda5d
viktor <888viktor2012@gmail.com>
2022-10-11 23:21:37 +0300

    login logout database functionality

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 application = Flask(__name__)
 application.config.from_object(Config)
 db = SQLAlchemy(application)
 migrate = Migrate(application, db)
-
-from app import routes
+login = LoginManager(application)
+from app import routes, models
+login.login_view = 'login'
