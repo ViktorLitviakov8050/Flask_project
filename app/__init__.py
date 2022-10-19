80ebbf225f117182214f914a572ce7cc501ec203
viktor <888viktor2012@gmail.com>
2022-10-19 23:02:23 +0300

    datebase, error handling, Profile page and avatars

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,27 @@ from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+import os
+import logging
+from logging.handlers import RotatingFileHandler
 
 application = Flask(__name__)
 application.config.from_object(Config)
 db = SQLAlchemy(application)
 migrate = Migrate(application, db)
 login = LoginManager(application)
-from app import routes, models
+from app import routes, models, errors
 login.login_view = 'login'
+
+if not application.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    application.logger.addHandler(file_handler)
+
+    application.logger.setLevel(logging.INFO)
+    application.logger.info('Microblog startup')
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,21 @@
-from app import application
-from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
+from app import application, db
+from flask import render_template, flash, redirect, url_for, request
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
 from flask_login import current_user, login_user, logout_user
 from app.models import User
+from flask_login import login_required
+from werkzeug.urls import url_parse
+from datetime import datetime
+
+@application.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @application.route('/')
 @application.route('/index')
 def index():
-    user = {'username': 'Miguel'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -18,11 +26,11 @@ def index():
             'body': 'The Avengers movie was so cool!'
         },
         {
-            'author': user,
+            'author': current_user,
             'body': 'The Python was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 @application.route('/login', methods=['GET', 'POST'])
 def login():
@@ -36,10 +44,94 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @application.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@application.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        flash("You're already registered!")
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login')), 201
+    return render_template('register.html', title='Register', form=form)
+
+@application.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    form = EmptyForm()
+    return render_template('user.html', user=user, posts=posts, form=form)
+
+@application.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
+
+
+@application.route('/follow/<username>', methods=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot follow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash('You are following {}!'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
+
+@application.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot unfollow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.unfollow(user)
+        db.session.commit()
+        flash('You are not following {}.'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
