90a31bde93c9b416e0d5e6b8289a1c069624c437
viktor <888viktor2012@gmail.com>
2022-11-14 21:37:40 +0000

    AJAX structureChapter 14: Ajax

---
+++ app/quote.py
@@ -0,0 +1,10 @@
+import json
+import requests
+from flask_babel import _
+from app import application
+
+def get_quote():
+    r = requests.get('https://zenquotes.io/api/random')
+    if r.status_code != 200:
+        return _('Quote service errrorrr')
+    return r.json()[0]["q"]
